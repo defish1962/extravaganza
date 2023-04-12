6097dbcdef94db24a0d2007b681801520377e994
David Fish <dave@ex3labs.com>
2023-04-12 12:51:55 -0600

    Add GraphQL API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "extravaganza": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "extravaganza006437f6": {
       "AppClientID": "string",
